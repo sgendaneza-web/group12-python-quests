26b86a96173f520ba1f21c798d2cd62493879f5e
gineza1-hash <g.ineza1@alustudent.com>
2026-02-24 15:17:53 +0200

    Superhero Commit

--- Quests/quest_02_naming_ceremony.py
+++ Quests/quest_02_naming_ceremony.py
@@ -1,4 +1,4 @@
 hero_name = "Blinkshift"
 superpower = "Teleportation"
 
-print(f"Meet {hero_name}, whose superpower is {superpower}!")
+print(f"Behold {hero_name}, whose superpower is {superpower}!")
