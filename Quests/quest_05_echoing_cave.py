b74102797913a59cf4edcf047f526f0aea79ad64
gineza1-hash <g.ineza1@alustudent.com>
2026-02-24 17:14:07 +0200

    Player Health Commit

---
+++ Quests/quest_05_echoing_cave.py
@@ -0,0 +1,6 @@
+player_health = 100
+#The monster attacks
+player_health = player_health - 25
+#The player finds a potion
+player_health = player_health + 10
+print(f"Final Player Health is {player_health}.")
